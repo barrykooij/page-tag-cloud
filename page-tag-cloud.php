7d01dd113e8ce96df652a4bac74226b0345af18a
Barry Kooij <git@barrykooij.nl>
2015-08-21 00:05:20 +0200

    plugin header and init via main function

--- page-tag-cloud.php
+++ page-tag-cloud.php
@@ -1,11 +1,27 @@
 <?php
 /*
-Plugin Name: Page Tag Cloud
-Plugin URI: http://www.barrykooij.com/page-tag-cloud/
-Description: Add tags to pages and display them in a tag cloud widget.
-Version: 2.0.0
-Author: Barry Kooij
-Author URI: http://www.barrykooij.com/
+	Plugin Name: Page Tag Cloud
+	Plugin URI: http://www.barrykooij.com/page-tag-cloud/
+	Description: Add tags to pages and display them in a tag cloud widget.
+	Version: 2.0.0
+	Author: Never5
+	Author URI: http://www.never5.com/
+
+	Page Tag Cloud Plugin
+	Copyright (C) 2012-2015, Never5 - www.never5.com
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 require_once( 'widget.php' );
@@ -81,4 +97,8 @@ class PageTagCloud {
 
 }
 
-add_action( 'plugins_loaded', create_function( '', 'new PageTagCloud();' ) );
+function __page_tag_cloud_main() {
+	new PageTagCloud();
+}
+
+add_action( 'plugins_loaded', '__page_tag_cloud_main', 20 );
